8585f143a5a2578e6753a743956e68254ce4574b
Carl Mai <carl.schoenbach@gmail.com>
2016-11-18 20:35:23 +0100

    tmdb uses requests now

--- tmdb.py
+++ tmdb.py
@@ -1,25 +1,26 @@
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 import json
+import requests
 
 key = '3e7807c4a01f18298f64662b257d7059'
 useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.41 Safari/537.1'
 
 def search(movie, year=None, max=None):
-	data = [ ( 'api_key' , key ), ('query' , movie ) ]
-	if year != None:
-		data.append( ( 'year' , year ) )
-	
-	data = urlencode(data)
-	url = 'http://api.themoviedb.org/3/search/movie?%s' % data
+    params = {
+        'api_key': key,
+        'query': movie,
+        'include_adult': 'true',
+    }
+    if year != None:
+        data['year'] = year
 
-	r = Request(url)
-	r.add_header('Accept','application/json')
-	r.add_header('User-Agent',useragent)
-
-	res = json.load( urlopen(r) )
-	
-	if max != None:
-		res = res[:5]
-
-	return res['results']
+    headers = {
+        'accept': 'application/json',
+        'user-agent': useragent
+    }
+    r = requests.get('http://api.themoviedb.org/3/search/movie', params=params, headers=headers)
+    res = r.json()
+    if max != None:
+        res = res[:5]
+    return res['results']
